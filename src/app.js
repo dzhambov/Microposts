e4dcf64bbf30176adf295a04e675cd7671d68ada
Hristo <owner@iMac.local>
2020-11-18 13:47:48 -0800

    add edit and update state with bugs.

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 // Listen for delete
 document.querySelector('#posts').addEventListener('click', deletePost);
 
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
+// Listen for edit cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 // Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -17,29 +23,48 @@ function getPosts() {
     .catch(err => console.log(err))
 } 
 
-// Add Post
+// Submit Post
 function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
 
   const data = {
     title,
     body
   }
 
-  // Create Post
-  http.post('http://localhost:3000/posts', data)
+  // Validate input
+  if(title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger');
+  } else {
+    
+    // Check for ID
+    if(id === '') {
+      // Create post
+      // Create Post
+    http.post('http://localhost:3000/posts', data)
     .then(data => {
       ui.showAlert('Post added', 'alert alert-success');
       ui.clearFields();
       getPosts();
     })
     .catch(err => console.log(err))
+    } else {
+      // Update Post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+      .then(data => {
+      ui.showAlert('Post updated', 'alert alert-success');
+      ui.changeFormState('add');
+      getPosts();
+    }) 
+    .catch(err => console.log(err))
+  }
 }
 
 // Delete Post
 function deletePost(e) {
-  e.preventDefault();
+  
   if(e.target.parentElement.classList.contains('delete')) {
     const id = e.target.parentElement.dataset.id;
     if(confirm('Are you sure you would like to delete this post?')) {
@@ -51,4 +76,34 @@ function deletePost(e) {
       .catch(err => console.log(err))
     }
   }
+  e.preventDefault();
+}
+
+// Enable Edit State
+function enableEdit(e) {
+  if(e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+
+
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // Fill form with current post
+    ui.fillForm(data);
+    
+  }
+  e.preventDefault();
+}
+
+// Cancel edit state
+function cancelEdit(e) {
+  if(e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add')
+  }
+  e.preventDefault();
 }
